c113744a76a76c25a9bb0d858135e71933677f83
Ali <kly441781@gmail.com>
2023-09-10 12:11:11 +0330

    added Serializer for Home Section

--- pictures/urls.py
+++ pictures/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
+    path('data/Home/Pictures/content/' , HomePictures_serializer),
+    path('data/Home/Info/content/' , HomeInfo_serializer),   
 ]
