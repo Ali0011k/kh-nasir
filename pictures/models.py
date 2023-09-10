c3088a8cbcf154087ac224ea014b1acdc59cc666
Ali <kly441781@gmail.com>
2023-09-10 11:54:23 +0330

    updated apps settings

--- pictures/models.py
+++ pictures/models.py
@@ -7,7 +7,11 @@ class HomePictures(models.Model):
     
     def __str__(self):
         return 'تصویر شماره {}'.format(self.pk)
-    
+    class Meta:
+        verbose_name = 'تصویر هنرستان'
+        verbose_name_plural = 'تصاویر هنرستان'
+        
+        
 class HomeInfo(models.Model):
     info = RichTextUploadingField(verbose_name='اطلاعات هنرستان')
     location = RichTextUploadingField(verbose_name='موقیعت مکانی هنرستان')
@@ -15,6 +19,8 @@ class HomeInfo(models.Model):
     slogan = models.CharField(max_length=200 , verbose_name=u"شعار هنرستان" ,default=None)
     def __str__(self) :
         return self.phone
-    
+    class Meta:
+        verbose_name = 'اطلاعات هنرستان'
+        verbose_name_plural = 'اطلاعات هنرستان'
     
     
